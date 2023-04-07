21535f1494b161ad8a87b795c10ab86ef2f430a8
Chic <42302404+laurendotcode@users.noreply.github.com>
2023-04-06 23:03:42 -0400

    fixed map

--- src/Form.tsx
+++ src/Form.tsx
@@ -2,19 +2,20 @@ import { TextInput } from "@mantine/core"
 import { useForm } from "@mantine/form"
 import { db } from "./firebase"
 import { collection, deleteDoc, doc, getDocs, setDoc } from "firebase/firestore"
+import { useEffect, useState } from "react"
 
 export function Form() {
-    
+    const [allDocs, setAllDocs] = useState<string[]>([])
     const form = useForm({
         initialValues: {
             name: ''
         }
     })
-    
+
     const name = form.getInputProps('name').value
     const MRN = form.getInputProps('MRN').value
 
-    const onSubmitHandler = async() => {
+    const onSubmitHandler = async () => {
         // Add a new document in collection "cities"
         await setDoc(doc(db, "Patients", name), {
             name: name,
@@ -23,40 +24,45 @@ export function Form() {
         });
     }
 
-    const deleteMatt = async() => {
-    await deleteDoc(doc(db, "Patients", "Matt"));}
+    /*const deleteMatt = async () => {
+        await deleteDoc(doc(db, "Patients", "Matt"));
+    }*/
 
-    async function getAllDocuments() {
-        const querySnapshot = await getDocs(collection(db, "Patients"));
-        const docsList: string[] = []
-        querySnapshot.forEach((doc) => { 
-            docsList.push(doc.id)
-        });
-        return docsList
-    }
+    useEffect(() => {
+        async function getAllDocuments() {
+            const querySnapshot = await getDocs(collection(db, "Patients"));
+            const docsList: string[] = []
+            querySnapshot.forEach((doc) => {
+                docsList.push(doc.id)
+            });
+            setAllDocs(docsList)
+        }
+        getAllDocuments();
+    })
 
 
     return (
         <div>
-        <form onSubmit={form.onSubmit((values) => onSubmitHandler())}>
-        <TextInput
-        withAsterisk
-        label="name"
-        placeholder="first name, last name"
-        {...form.getInputProps('name')}
-        />
-        <TextInput
-        withAsterisk
-        label="MRN"
-        placeholder=""
-        {...form.getInputProps('MRN')}
-        />
-        <button type="submit">Submit</button>
-        </form>
-        <button onClick={deleteMatt}>Delete Matt</button>
-        <ol>
-        {getAllDocuments().map((doc: string)=><li>{doc}</li>)}
-        </ol>
+            <form onSubmit={form.onSubmit((values) => onSubmitHandler())}>
+                <TextInput
+                    withAsterisk
+                    label="name"
+                    placeholder="first name, last name"
+                    {...form.getInputProps('name')}
+                />
+                <TextInput
+                    withAsterisk
+                    label="MRN"
+                    placeholder=""
+                    {...form.getInputProps('MRN')}
+                />
+                <button type="submit">Submit</button>
+            </form>
+            <button onClick={deleteMatt}>Delete Matt</button>
+            <ol>
+                {allDocs.map((doc: string) => <li>{doc}</li>)}
+            </ol>
         </div>
-        )
+
+    )
 }
